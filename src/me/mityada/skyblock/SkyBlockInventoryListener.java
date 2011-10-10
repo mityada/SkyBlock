29cd4253ea0653fd65e8181194794470540040d8
Dmitry Deshevoy <mityada@gmail.com>
2011-10-10 20:27:57 +0400

    License added

--- src/me/mityada/skyblock/SkyBlockInventoryListener.java
+++ src/me/mityada/skyblock/SkyBlockInventoryListener.java
@@ -1,9 +1,26 @@
+/*
+ * Bukkit server plugin based on the Noobcrew's SkyBlock map.
+ * Copyright (C) 2011 Dmitry Deshevoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package me.mityada.skyblock;
 
 import java.io.File;
 
 import org.bukkit.World;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.config.Configuration;
